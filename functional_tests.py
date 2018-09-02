843158cd038d0a9a840f26410303581e06985f70
Matt Mackowski <matt@mackowski.com>
2018-09-02 14:35:14 -0500

    Second test completed

--- functional_tests.py
+++ functional_tests.py
@@ -1,10 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.firefox.options import Options
+import unittest
 
-cap = DesiredCapabilities().FIREFOX
-cap["marionette"] = False
-browser = webdriver.Firefox(capabilities=cap, executable_path="geckodriver.exe")
-# browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        cap = DesiredCapabilities().FIREFOX
+        cap["marionette"] = True
+        opts = Options()
+        opts.log.level = "trace"
+        self.browser = webdriver.Firefox( executable_path="./geckodriver.exe", capabilities=cap, options=opts)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+    # assert 'To-Do' in browser.title, "Browser title was " + browser.title
+
+# browser.quit()
